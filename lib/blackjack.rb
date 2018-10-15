464de6fe5ec8a454c6a9ecf6d86f0f1af57a2c68
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-15 04:51:28 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,20 +68,6 @@ def runner
   # code runner here
  
   welcome
-  initial_round
   
-  hit?(card_total)
-  
-  until card_total > 21
-  card_total = card_total + hit?(card_total)
-  display_card_total(card_total)
-
-  
-  
-  
-  end
-end_game (card_total)
-  
- #
 end
     
