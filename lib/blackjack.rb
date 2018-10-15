271dcf7426cba59227f740b37242a80654110699
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-15 04:02:44 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,8 +33,8 @@ end
 
 def initial_round
   # code #initial_round here
-  return deal_card + deal_card
-  puts display_card_total(card_total)
+  
+  puts display_card_total(deal_card + deal_card)
 end
 
 def hit?
