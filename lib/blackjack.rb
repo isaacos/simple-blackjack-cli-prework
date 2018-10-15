15856ed9f0227bb3a237b41fb42955b66f89b705
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-15 03:40:09 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,11 +10,14 @@ end
 
 def display_card_total (card_total)
   # code #display_card_total here
-  print deal_card
+  print 'Your cards add up to #{card_total}'
 end
 
 def prompt_user
   # code #prompt_user here
+  
+  puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def get_user_input
